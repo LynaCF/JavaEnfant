6f6591f953e85c2126e461c014934af91f0ebbda
LynaCF <lyna.chaim@gmail.com>
2019-01-25 10:04:14 +0100

    first commit

---
+++ BonjourMonde.java
@@ -0,0 +1,12 @@
+public class BonjourMonde {
+
+  public static void main(String[] args) {
+    System.out.println("BonjourMonde");
+	mangerglace("Fraise","Vanille","Chocolat");
+  }
+
+
+public static void mangerglace(String p1 ,String p2 ,String p3 ){
+System.out.println("Je mange une glace parfum : " + p1 + " " + p2 +" "+p3);
+}
+}
